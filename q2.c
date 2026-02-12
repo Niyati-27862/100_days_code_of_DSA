7914ceb8688cee4afe9444bc0f12cabc38e52b04
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-02-12 19:17:40 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, target;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                printf("Indices: %d %d\n", i, j);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
