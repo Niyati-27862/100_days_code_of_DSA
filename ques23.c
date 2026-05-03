92a0af3d014590bf765b51d8f3a4caa69d1b1b59
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 11:06:40 +0530

    Add files via upload

---
+++ ques23.c
@@ -0,0 +1,39 @@
+// Check whether a matrix is symmetric
+
+#include <stdio.h>
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[100][100]
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    if(m != n) {
+        printf("Not a Symmetric Matrix\n");
+        return 0;
+    }
+
+    int isSymmetric = 1;
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if(isSymmetric == 0)
+            break;
+    }
+
+    if(isSymmetric)
+        printf("Symmetric Matrix\n");
+    else
+        printf("Not a Symmetric Matrix\n");
+
+    return 0;
+}
