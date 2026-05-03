ac84fa1d1ccf131cdfa523e35058769ec0633521
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 11:17:42 +0530

    Add files via upload

---
+++ ques35.c
@@ -0,0 +1,29 @@
+// Problem: Given an array of integers, rotate the array to the right by k positions.
+#include <stdio.h>
+int main() {
+    int n, k;
+
+    scanf("%d", &n);
+    int nums[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[n];
+    for(int i = 0; i < n; i++) {
+        temp[(i + k) % n] = nums[i];
+    }
+    for(int i = 0; i < n; i++) {
+        nums[i] = temp[i];
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
