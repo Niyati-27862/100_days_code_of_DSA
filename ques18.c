722e074f2efab2c70c6747bbc89a12cce4793182
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 11:00:02 +0530

    Add files via upload

---
+++ ques18.c
@@ -0,0 +1,26 @@
+// Reverse a string using in-place two-pointer approach
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+
+    scanf("%s", s);
+
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while(left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    printf("%s\n", s);
+
+    return 0;
+}
