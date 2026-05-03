e268e1ea7cf8afb61ec26c2d4381bf0e75656b74
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 12:06:51 +0530

    Add files via upload

---
+++ ques46.c
@@ -0,0 +1,49 @@
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Detect cycle using Floyd’s algorithm
+bool hasCycle(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          
+        fast = fast->next->next;    
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main() {
+
+    struct Node* head = createNode(3);
+    head->next = createNode(2);
+    head->next->next = createNode(0);
+    head->next->next->next = createNode(-4);
+
+    head->next->next->next->next = head->next;
+
+    if (hasCycle(head))
+        printf("true");
+    else
+        printf("false");
+
+    return 0;
+}
