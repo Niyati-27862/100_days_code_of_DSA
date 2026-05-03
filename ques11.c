b1e1e31a91fd170362f9db125f28d19f2180ce9b
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 10:35:37 +0530

    Add files via upload

---
+++ ques11.c
@@ -0,0 +1,28 @@
+// Remove duplicates from a sorted array and print unique elements
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print first element
+    if(n > 0) {
+        printf("%d ", arr[0]);
+    }
+
+    // Check for unique elements
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[i - 1]) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
