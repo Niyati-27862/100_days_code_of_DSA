e1aa2a1817df01bf5c4e53d10e2af59bea2361a7
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 11:01:37 +0530

    Add files via upload

---
+++ ques19.c
@@ -0,0 +1,29 @@
+// Check if a string is a palindrome using two-pointer approach
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+
+    int left = 0;
+    int right = strlen(str) - 1;
+    int isPalindrome = 1;
+
+    while(left < right) {
+        if(str[left] != str[right]) {
+            isPalindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if(isPalindrome)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
