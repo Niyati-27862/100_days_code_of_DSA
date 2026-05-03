1b305922c8799745c56906fbd1dd76600ccca093
Niyati-27862 <Niyati.27862@stu.upes.ac.in>
2026-05-03 11:11:01 +0530

    Add files via upload

---
+++ ques27.c
@@ -0,0 +1,44 @@
+// Check whether a matrix is an Identity Matrix
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[100][100];
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isIdentity = 1;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) {
+                if(matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } else {
+                if(matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+        if(isIdentity == 0)
+            break;
+    }
+
+    if(isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
